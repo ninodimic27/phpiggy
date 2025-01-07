fecb4812b1339ce526e70d8d908d1296e032478b
Nino <ndpromarketing@gmail.com>
2025-01-07 17:43:29 +0100

    Router work

---
+++ src/Framework/Router.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Framework;
+
+class Router 
+{
+    private array $routes = [];
+
+    public function add(string $path)
+    {
+        $this->routes[] = 
+        [
+            'path' => $path
+        ];
+    }
+}
